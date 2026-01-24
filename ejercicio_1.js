3307fa8fb742cb1902c31c2c415f93773db2ac69
Belo <martinvecinoabel@gmail.com>
2026-01-24 13:57:31 -0500

    Ejercicio 1 terminado.

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+    function colorAleatorio() {
+        let red = Math.floor(Math.random() * 255)
+        let green = Math.floor(Math.random() * 255)
+        let blue = Math.floor(Math.random() * 255)
+        
+        return `rgb(${red}, ${green}, ${blue})`
+    }
+    
     let button = document.querySelector('[class="ej1 button"]')
-    console.log(button)
+    button.addEventListener('click', () => {
+        document.body.style.backgroundColor = colorAleatorio()
+    })
 })
