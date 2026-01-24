267d618d98057c35209c51a9f093ada3c881be93
Belo <martinvecinoabel@gmail.com>
2026-01-24 14:20:11 -0500

    Ejercicio 2 terminado.

--- ejercicio_2.js
+++ ejercicio_2.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let cantClics = 0
+    let text = document.getElementById('clics')
+    let button = document.querySelector('.button')
     
+    button.addEventListener('click', () => {
+        cantClics += 1
+        text.textContent = `Clics: ${cantClics}`
+    })
 })
