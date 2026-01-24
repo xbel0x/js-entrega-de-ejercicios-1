829a4bf8ff46d5a499b6e93ba0b3627efa7693f1
Belo <martinvecinoabel@gmail.com>
2026-01-24 14:22:28 -0500

    Correcccion en ejercicio 1.

--- ejercicio_1.js
+++ ejercicio_1.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function colorAleatorio() {
-        let red = Math.floor(Math.random() * 255)
-        let green = Math.floor(Math.random() * 255)
-        let blue = Math.floor(Math.random() * 255)
-        
-        return `rgb(${red}, ${green}, ${blue})`
-    }
+function colorAleatorio() {
+    let red = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
     
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+document.addEventListener('DOMContentLoaded', () => {    
     let button = document.querySelector('[class="ej1 button"]')
     button.addEventListener('click', () => {
         document.body.style.backgroundColor = colorAleatorio()
