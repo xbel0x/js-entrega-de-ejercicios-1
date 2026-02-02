ec1e378f3ecc9520241cdfa6353b39093de9e35d
Belo <martinvecinoabel@gmail.com>
2026-02-01 23:49:04 -0500

    Ejercicio 7 terminado.

--- ejercicio_7.js
+++ ejercicio_7.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const generarClaveAleatoria = (len) => {
+        const minusculas = 'abcdefghijklmnopqrstuvwxyz'.split('');
+        const mayusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+        const numeros = '0123456789'.split('')
+        const especiales = '!@#$%^&*()_+-=[]{}|;:,.<>?'.split('');
+        
+        let pass = ''
+        let tiposDeCaracteres = [minusculas, mayusculas, numeros, especiales]
+        
+        for (let i = 0; i < len; i++) {
+            let tipoDeCaracter = tiposDeCaracteres[Math.floor(Math.random() * tiposDeCaracteres.length)]
+            let caracter = tipoDeCaracter[Math.floor(Math.random() * tipoDeCaracter.length)]
+            pass = pass.concat(caracter)           
+        }
+        return pass
+    }
+
+    let gen = document.getElementById('generar')
+    let pass = document.getElementById('pass')
     
+    gen.addEventListener('click', () => {
+        let len = document.getElementById('longitud').value
+        if (parseInt(len) < 4 || len == '') {
+            alert('La longitud debe ser mayor o igual a 4.')
+        } else {
+            pass.textContent = `Su contraseña es: ${generarClaveAleatoria(len)}`
+        }
+    })
 })
